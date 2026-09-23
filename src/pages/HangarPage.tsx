c2e452ba1123f728fc71a29549c43d525d6f48c2
Aliyu Usman <aliyuusman4109@gmail.com>
2026-09-23 21:23:16 +0100

    Update HangarPage.tsx

--- src/pages/HangarPage.tsx
+++ src/pages/HangarPage.tsx
@@ -1166,7 +1166,7 @@ export default function HangarPage() {
 
               {/* Pilot is completely independent and stands
                   on the grass shoulder beside the road */}
-<HangarMan position={[1.5, 0, 1.1]} /></Suspense>
+<HangarMan position={[1.5, 0, 0.5]} /></Suspense>
 
             {/* ==================================================
                 CAMERA CONTROL
