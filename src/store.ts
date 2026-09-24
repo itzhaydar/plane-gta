503264bcee940017075c18f6e20576b70a8c4b5b
Aliyu Usman <aliyuusman4109@gmail.com>
2026-09-24 01:02:11 +0100

    Add role management to plane store state

--- src/store.ts
+++ src/store.ts
@@ -9,21 +9,44 @@ export type LastRun = {
   score: number;
 } | null;
 
+export type Role = 'pilot' | 'homie';
+
 type State = {
   liveries: Liveries;
   activeFace: Face;
   lastRun: LastRun;
+  role: Role | null;
+
   setActiveFace: (face: Face) => void;
   saveFace: (face: Face, dataUrl: string) => void;
   setLastRun: (run: LastRun) => void;
+  setRole: (role: Role) => void;
 };
 
 export const usePlaneStore = create<State>((set) => ({
-  liveries: { top: null, bottom: null, left: null, right: null, flag: null },
+  liveries: {
+    top: null,
+    bottom: null,
+    left: null,
+    right: null,
+    flag: null,
+  },
+
   activeFace: 'top',
   lastRun: null,
+  role: null,
+
   setActiveFace: (face) => set({ activeFace: face }),
+
   saveFace: (face, dataUrl) =>
-    set((s) => ({ liveries: { ...s.liveries, [face]: dataUrl } })),
+    set((s) => ({
+      liveries: {
+        ...s.liveries,
+        [face]: dataUrl,
+      },
+    })),
+
   setLastRun: (lastRun) => set({ lastRun }),
+
+  setRole: (role) => set({ role }),
 }));
